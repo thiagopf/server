784331edde0b2163c7083165f84d00a27bdc27dc
Thiago <thiago.fernandes210@gmail.com>
2018-07-19 13:57:42 -0300

    Use PORT from environment.

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -43,6 +43,7 @@
 
 import http.server
 import requests
+import os
 from urllib.parse import unquote, parse_qs
 
 memory = {}
@@ -148,6 +149,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
                 "Couldn't fetch URI '{}'. Sorry!".format(longuri).encode())
 
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    port = int(os.environ.get('PORT', 8000))
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
